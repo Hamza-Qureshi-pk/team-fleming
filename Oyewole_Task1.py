6b15c815ba3f2f3daffc824f3dd8f4e2d0bb8e96
Oyewole Oluwaseyi <34714137+seyi5488@users.noreply.github.com>
2020-08-03 17:31:51 +0100

    Add files via upload

---
+++ Oyewole_Task1.py
@@ -0,0 +1,5 @@
+print ("Name: Oyewole Oluwaseyi")
+print ("Email: oluwaseyioyewole03@gmail.com")
+print ("Language: Python")
+print ("Biostack: Drug development")
+print ("Slack Username: @Oyewole")
