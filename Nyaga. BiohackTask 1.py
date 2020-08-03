dd0849da11c0388fcc0ab34e948c0148ee50ab4a
nellyflashier <38782969+nellyflashier@users.noreply.github.com>
2020-08-03 21:47:51 +0300

    Add files via upload

---
+++ Nyaga. BiohackTask 1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 17:59:40 2020
+
+@author: hp
+"""
+
+#Name of user
+print ("NAME: Nelly Nyaga")
+#email Adress
+print ("EMAIL: nellyflashier@gmail.com")
+#language used
+print ("LANGUAGE: Python 3.7")
+#Biostack
+print ("BIOSTACK: Drug development, team fleming")
+#slack username
+print("SLACK USERNAME: @nyaga")
