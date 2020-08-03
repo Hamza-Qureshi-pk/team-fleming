599ae256c40d654b743cd0ce358721b00d9846f3
Qamar Zaman <68494521+king007-me@users.noreply.github.com>
2020-08-03 22:42:32 +0500

    Task1 Done by @Qamar

---
+++ Task1_script.py
@@ -0,0 +1,10 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: C:\Users\Hacker\Desktop\Task1_script.py ===============
+Name: Qamar Zaman
+Email: khanqamr278@gmail.com
+Language: Python
+Biostack Name: Genomic
+Slack Name: @Qamar
+>>> 
