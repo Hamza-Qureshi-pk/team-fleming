c60c84d7cc4075a8c8812845b3006774e72d2e30
Qamar Zaman <68494521+king007-me@users.noreply.github.com>
2020-08-04 18:41:09 +0500

    Delete Hackbio_Task_1.pl

--- Hackbio_Task_1.pl
+++
@@ -1,6 +0,0 @@
-$name = 'Vishnupriya T';
-$mail_id = 'vishnupriya99342@gmail.com';
-$language = 'PERL';
-$biostack = 'Drug Development';
-$slack_username = '@Vishnu';
-print("Name: $name \nMail ID: $mail_id \nLanguage: $language \nBiostack: $biostack \nSlack Username: $slack_username");
