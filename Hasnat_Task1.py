8c588f60796afb53b399f1925086faf1c1140b51
Hasnat Noor <62334471+HasnatNoor@users.noreply.github.com>
2020-08-04 19:10:10 +0500

    Add files via upload

---
+++ Hasnat_Task1.py
@@ -0,0 +1,10 @@
+name="Hasnat Noor"
+email="hasnatmalik7127@gmail.com"
+lang="Python"
+bioSt="Drug Development"
+slack="@Hasnat"
+print("My Name is", name )
+print("My email address is", email)
+print("I'm writing this introduction using", lang)
+print("My Biostack is", bioSt)
+print("My slack username is",  slack)
