7d172f1a209349e6679f2c5ee59d7b3dcfc6459c
Vishnupriya2730 <69183801+Vishnupriya2730@users.noreply.github.com>
2020-08-04 18:30:08 +0530

    Hackbio_Task_1

---
+++ Hackbio_Task_1.pl
@@ -0,0 +1,5 @@
+print "Name: Vishnupriya T\n";
+print "Mail ID: vishnupriya99342@gmail.com\n";
+print "Language Used: PERL\n";
+print "Biostack: Drug Development\n";
+print "Biostack Username: @Vishnu\n";
