9e735c9c1bf8550641cac344f23617edc5b9008d
Qamar Zaman <68494521+king007-me@users.noreply.github.com>
2020-08-04 18:10:30 +0500

    Delete Hackbio_Task_1.pl

--- Hackbio_Task_1.pl
+++
@@ -1,5 +0,0 @@
-print "Name: Vishnupriya T\n";
-print "Mail ID: vishnupriya99342@gmail.com\n";
-print "Language Used: PERL\n";
-print "Biostack: Drug Development\n";
-print "Biostack Username: @Vishnu\n";
