5a30be2fcc8d648010db90535009eb218c23feb3
SamB-stack <69101360+SamB-stack@users.noreply.github.com>
2020-08-04 23:30:07 +0100

    Add files via upload

---
+++ SamB.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "SamB" << endl;
+    cout << "Biostack - Drug Development" << endl;
+    cout << "   /" << endl;
+    cout << "  /" << endl;
+
+    cout << "Drug Development is an epitome of pharmaceutical science required in the discovery, design and modification of new and existing medications. It is very vital in a time like this where the world is posed with a virus known as Covid-19" << endl;
+    return 0;
+}
