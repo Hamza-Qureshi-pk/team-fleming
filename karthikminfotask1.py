5d96d470b5773586f74fa502c8c2b5967669849f
Karthikminfo <69101599+Karthikminfo@users.noreply.github.com>
2020-08-05 10:56:50 +0530

    Add files via upload

--- karthikminfotask1.py
+++ karthikminfotask1.py
@@ -1,10 +1,10 @@
 Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
-=== RESTART: D:\Drug discovery hackethon\Python script\karthickminfotask1.py ===
+============= RESTART: C:\Users\Admin\Desktop\karthikminfotask1.py =============
 Name: Karthikeyan
 Email: karthikeyan_imbsf@tcarts.in
 Language: Python
 Biostack Name: Drug discovery and Vaccine informatics
-slack name: Karthikeyan Mahendhran
+slack name: Karthikeyan
 >>> 
