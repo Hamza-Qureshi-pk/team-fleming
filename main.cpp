84215e2992a74088b429d2a9222d64d100d17ef2
Qamar Zaman <68494521+king007-me@users.noreply.github.com>
2020-08-05 20:11:18 +0500

    Done By @Qamar

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "***********************************" << endl;
+    cout<<"Name: Qamar Zaman"<<endl;
+    cout<<"Email: Khanqamar278@gmail.com"<<endl;
+    cout<<"Language: C++"<<endl;
+    cout<<"Biostack Name: Genomics"<<endl;
+    cout<<"Slack Name: @Qamar"<<endl;
+    cout<<"***********************************";
+    return 0;
+}
