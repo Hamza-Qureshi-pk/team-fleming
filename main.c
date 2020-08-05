6f888a864632f78b471b29d26f8343a3668f5394
Qamar Zaman <68494521+king007-me@users.noreply.github.com>
2020-08-05 18:59:06 +0500

    Done By @Qamar

---
+++ main.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+int main()
+{
+    printf("**************************************\n");
+    printf("Name: Qamar Zaman\n");
+    printf("Email: Khanqamar278@gmail.com\n");
+    printf("Language: C language\n");
+    printf("Biostack Name: Genomics\n");
+    printf("Slack Name: @Qamar\n");
+    printf("**************************************\n");
+    return(0);
+}
