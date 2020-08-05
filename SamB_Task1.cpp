1093858419cdc118216482d893165944a34496de
Hamza Qureshi <49978636+HamzaQureshi21@users.noreply.github.com>
2020-08-05 15:46:37 +0500

    Update SamB_Task1.cpp

--- SamB_Task1.cpp
+++ SamB_Task1.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Samuel Jacob Bunu, pharmsamuelbunu@gmail.com, C++, Drug development" << endl;
-    cout << "@+SamB" << endl;
+    cout << "@SamB" << endl;
     return 0;
 }
