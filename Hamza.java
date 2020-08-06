565d1e2499cacda137f875959d3d3445a2f0349c
Hamza Qureshi <49978636+HamzaQureshi21@users.noreply.github.com>
2020-08-06 13:11:49 +0500

    Create Hamza.java

---
+++ Hamza.java
@@ -0,0 +1,6 @@
+public class Hamza{
+
+     public static void main(String []args){
+        System.out.println("Hamza Qureshi   hamza3371qureshi@gmail.com  Java    Drug Development    @Hamza");
+     }
+}
