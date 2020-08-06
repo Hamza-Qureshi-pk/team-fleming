2a2a897f942f2b47c334ec82b3ffb66bea96d8fd
Hamza Qureshi <49978636+HamzaQureshi21@users.noreply.github.com>
2020-08-06 16:56:26 +0500

    Delete Hamza.java

--- scripts/Hamza.java
+++
@@ -1,6 +0,0 @@
-public class Hamza{
-
-     public static void main(String []args){
-        System.out.println("Hamza Qureshi   hamza3371qureshi@gmail.com  Java    Drug Development    @Hamza");
-     }
-}
