36060da671a1195ae185bc7d47bc133383f36117
kb7-ops <69096180+kb7-ops@users.noreply.github.com>
2020-08-06 10:53:57 +0500

    added java file

---
+++ Intrnship.java
@@ -0,0 +1,15 @@
+package HackBioAssignment;
+
+public class Intrnship {
+
+	public static void main(String[] args) {
+		System.out.println("Hack bio assignment");
+		System.out.println("Mazhar Khan");
+		System.out.println("mazharshaheen027@gamil.com");
+		System.out.println("Java");
+		System.out.println("Drug development");
+		System.out.println("@Mazhar");
+
+	}
+
+}
