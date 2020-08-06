25c5b8100347aa5980489e4d4402ec11d82982b9
Olubunmiwewe <69095281+Olubunmiwewe@users.noreply.github.com>
2020-08-06 15:17:06 +0100

    Add files via upload

---
+++ scripts/Olubunmi_Wewe_task1.py
@@ -0,0 +1,9 @@
+print ("Name- OLUBUNMI ABIODUN WEWE")
+
+print ("Email- olubunmiwewe@gmail.com")
+
+print ("Language- Python 3.8 64-bit")
+
+print ("BioStack- Drug Development")
+
+print ("Slack Username- @Bunmi")
