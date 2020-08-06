1c5d934e415f28dfa242c27cbb0aef90a6c4ec2e
Hamza Qureshi <49978636+HamzaQureshi21@users.noreply.github.com>
2020-08-06 16:34:43 +0500

    Update and rename Hamza_team-fleming.cpp to Hamza.java

--- scripts/Hamza_team-fleming.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-main()
-{ 
-cout<<"Team Lead @ #team-fleming\n Name: Hamza Qureshi\n Email: hamza3371qureshi@gmail.com\n Language used: C++\n Biostack: Drug Development\n  Slack Username: @Hamza";
-return 0;
-}
