1e1c4174c1fa9de910f3b90ec59b6b97537364cc
Qamar Zaman <khanqamar278@gmail.com>
2020-08-09 21:05:55 +0500

    delete

--- scriptss/Hackbio_Task.rb
+++
@@ -1,5 +0,0 @@
-puts "Name:Pavithra Elumalai\n";
-puts "Mail ID:ev.pavithra@gmail.com\n";
-puts "Language Used:RUBY\n";
-puts "Biostack:GENOMICS\n";
-puts "Slack User Name:@Pavi\n";
--- scriptss/TaskScript_1.R
+++
@@ -1,5 +0,0 @@
-cat("Name: Tasmina Ferdous Susmi")
-cat("Email: tsusmi36@yahoo.com")
-cat("Language: R")
-cat(" Biostack: Drug Development")
-cat("slack username: @Susmi")
