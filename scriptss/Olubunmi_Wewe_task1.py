0ace082daccd59b1f90ff1a4a6f1116b98d3d9f2
Qamar Zaman <khanqamar278@gmail.com>
2020-08-10 14:30:32 +0500

    delete scriptss and team collab files

--- scriptss/Olubunmi_Wewe_task1.py
+++
@@ -1,9 +0,0 @@
-print ("Name- OLUBUNMI ABIODUN WEWE")
-
-print ("Email- olubunmiwewe@gmail.com")
-
-print ("Language- Python 3.8 64-bit")
-
-print ("BioStack- Drug Development")
-
-print ("Slack Username- @Bunmi")
