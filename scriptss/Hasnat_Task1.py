d9581d337348824cd99fdfcc2034d646ad1fa007
Qamar Zaman <68494521+king007-me@users.noreply.github.com>
2020-08-10 11:35:08 +0500

    Add files via upload

---
+++ scriptss/Hasnat_Task1.py
@@ -0,0 +1,13 @@
+
+name="Hasnat Noor"
+email="hasnatmalik7127@gmail.com"
+lang="Python"
+bioSt="Drug Development"
+slack="@Hasnat"
+
+Task={"Name":name,"Email":email,"Language":lang,"Biostack Name":bioSt,"Slack Name":slack}
+import pandas as pd
+from pandas import DataFrame
+Result=(Task)
+print(Result)
+
