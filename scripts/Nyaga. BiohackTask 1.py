59224c821a4bc351c59a37d447579e776594a95a
Qamar Zaman <68494521+king007-me@users.noreply.github.com>
2020-08-10 16:40:04 +0500

    Add files via upload

---
+++ scripts/Nyaga. BiohackTask 1.py
@@ -0,0 +1,8 @@
+#My first program in python 
+name='Nelly Nyaga'
+email='nellyflashier@gmail.com'
+language='Python 3.7'
+biostacks='Drug development'
+username='@Nyaga'
+
+print(name+','+email+','+language+','+biostacks+','+username)
