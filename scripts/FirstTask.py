9113af54d601fca16f12e961b4d587513200b2fe
Qamar Zaman <68494521+king007-me@users.noreply.github.com>
2020-08-10 18:26:10 +0500

    Add files via upload

---
+++ scripts/FirstTask.py
@@ -0,0 +1,9 @@
+
+#HackBio Firsttask to print items
+name="Oluwatobi Oluremi Bamidele"
+email="Email:mumdees1@gmail.com"
+lang="Python"
+bioSt="Bioinformatics  frontend/UI/UX"
+slack="@Oluremi"
+
+print(name+','+email+','+lang+','+bioSt+','+slack)
